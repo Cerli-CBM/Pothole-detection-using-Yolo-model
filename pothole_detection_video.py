a48d733a97ca7732ed87c18462e10b01df6aec3f
Cerli C B M <145151806+Cerli-CBM@users.noreply.github.com>
2025-12-05 21:56:18 +0530

    Add files via upload

---
+++ pothole_detection_video.py
@@ -0,0 +1,53 @@
+import cv2
+import math
+import cvzone
+from ultralytics import YOLO
+
+# Load video
+video_path = "pothole_Video.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Load model
+model = YOLO("best.pt")
+
+# Class names
+classNames = ['Pothole']
+
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")   
+fps = int(cap.get(cv2.CAP_PROP_FPS))       
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+out = cv2.VideoWriter("output_pothole_detection.mp4", fourcc, fps, (width, height))
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
+                               (max(0, x1), max(35, y1)), scale=1, thickness=1)
+
+    out.write(img)
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release everything
+cap.release()
+out.release()
+cv2.destroyAllWindows()
